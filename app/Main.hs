0d39c8aff80f1c5ad3662ce2b8a5cce6a4da9992
Wilmer Nilsson <wlmr@pm.me>
2018-12-01 09:24:08 +0100

    Done for the day

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
-
+import Data.Maybe
+import System.Environment
 import Lib
 
+fDict = [("d1",d1),("d1b",d1b)]
+
 main :: IO ()
-main = interact d1 
+main = do
+    (day:args) <- getArgs
+    let (Just func) = lookup day fDict
+    interact func
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,18 @@
 module Lib
     ( d1
+    , d1b
     ) where
 
 d1 :: String -> String
-d1 strs = strs ++ "fucc"
+d1 = show . sum . map readSigned . lines 
+        where readSigned sInt = if '+' == head sInt then (read . tail) sInt else read sInt
 
+d1b :: String -> String
+d1b = show . firstToTwo [0] 0 . cycle . map readSigned . lines 
+        where readSigned signInt = if '+' == head signInt then (read . tail) signInt else read signInt
+
+--helper for d1b
+firstToTwo :: [Int] -> Int -> [Int] -> Int
+firstToTwo temp val []     = 0
+firstToTwo temp val (x:xs) = if x' `elem` temp then x' else firstToTwo (x':temp) x' xs 
+        where x' = val+x 
