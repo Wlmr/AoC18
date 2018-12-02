53558929da845a683a52e3cc4bff1c000958a22d
Wilmer Nilsson <wlmr@pm.me>
2018-12-02 16:57:35 +0100

    d2b done

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,7 +8,6 @@ module Lib
 import Data.List
 
 -- DAY 1:
-
 d1a :: String -> String
 d1a = show . sum . map readSigned . lines 
 
@@ -24,7 +23,6 @@ firstDuplicate temp val (x:xs) = if x' `elem` temp then x' else firstDuplicate (
         where x' = val+x 
 
 -- DAY 2:
-
 d2a :: String -> String
 d2a = show . uncurry (*) . foldl1 (\(a,b) (x,y) -> (a+x,b+y)) . map (foldl twoAndThreeCounter (0,0) . map length . group . sort) . lines
 
@@ -34,4 +32,21 @@ twoAndThreeCounter (two,    0) 3 = (two,1)
 twoAndThreeCounter (two,three) _ = (two,three) 
 
 d2b :: String -> String
-d2b s = s
+d2b s = closestMatch (x:xs) xs ++ "\n"
+    where (x:xs) = lines s
+
+closestMatch :: [String] -> [String] -> String
+closestMatch (x:xs) []     = closestMatch xs xs
+closestMatch (x:xs) (y:ys) = if jämförare x y == 1 then commonLetters x y else closestMatch (x:xs) ys
+
+jämförare :: String -> String -> Int
+jämförare (x:xs) (y:ys)
+    | x == y    = 0 + jämförare xs ys
+    | otherwise = 1 + jämförare xs ys
+jämförare _ _ = 0
+
+commonLetters :: String -> String -> String
+commonLetters (x:xs) (y:ys)
+    | x == y    = x : commonLetters xs ys
+    | otherwise = "" ++ commonLetters xs ys
+commonLetters _ _ = ""
